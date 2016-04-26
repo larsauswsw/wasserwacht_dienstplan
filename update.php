76404a2e285499934026999e638e65379b7236c5
Lars Miesner <larsauswsw@gmail.com>
2016-04-26 16:15:13 +0200

    Email eingebaut

--- update.php
+++ update.php
@@ -24,12 +24,20 @@ $dbname = $settings['db']['dbname'];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+$email_from = $settings['email']['from'];
+$email_to = $settings['email']['to'];
+$email_subject = $settings['email']['subject'];
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $changes = false;
+$email_body = "Hallo,\n\nes gibt neue Änderungen im Dienstplan:\n\nTag - Ort - Alter Wert - Neuer Wert\n";
+$email_header = "From: Lars Miesner <" . $email_from . ">\r\n";
+$email_header .= "Mime-Version: 1.0\r\n";
+$email_header .= "Content-type: text/plain; charset=utf-8";
 
 foreach ($_POST['tage'] as $tag => $value){
     #echo "Datum : " . $tag . "<br>";
@@ -49,7 +57,7 @@ foreach ($_POST['tage'] as $tag => $value){
 	    } else {
 	        exit("Fehler beim Speichern der Daten: " . $conn->error);
 	    }
-	    
+	    $email_body .= " - ". $tag . " - " . $ort . " - " . $oldValue . " - " . $inputValue . "\n"; 
 	    $sqlInsert = "INSERT INTO auditlog (tag,ort,alter_wert,neuer_wert,zeitpunkt) VALUES ('". $tag . "','" . $ort . "','" . $oldValue . "','" . $inputValue . "',NOW());";
             if ($conn->query($sqlInsert) === TRUE) {
                 #echo "Record updated successfully";
@@ -60,40 +68,12 @@ foreach ($_POST['tage'] as $tag => $value){
     }
 }
 
-/*
-foreach ($_POST['ferien'] as $tag => $value){
-    #echo "Datum : " . $tag . "<br>";
-    foreach($value as $ort => $inputValue){
-        #echo "Ort         : " . $ort . "<br>";
-        #echo "Input Value : " . $inputValue . "<br>";
-
-        $selectSql = "SELECT " . $ort . " FROM ferien where tag='" . $tag . "';";
-        $oldValueResult = $conn->query($selectSql);
-        $oldValueResult = $oldValueResult->fetch_assoc();
-        $oldValue = $oldValueResult[$ort];
-        if ($inputValue != $oldValue) {
-	    $changes = true;
-            $sqlUpdate = "UPDATE ferien SET " . $ort . "='" . $inputValue . "' WHERE tag='" . $tag . "';";
-            if ($conn->query($sqlUpdate) === TRUE) {
-                #echo "Record updated successfully";
-            } else {
-                exit("Fehler beim Speichern der Daten: " . $conn->error);
-            }
-
-            $sqlInsert = "INSERT INTO auditlog (tag,ort,alter_wert,neuer_wert,zeitpunkt) VALUES ('". $tag . "','" . $ort . "','" . $oldValue . "','" . $inputValue . "',NOW());";
-            if ($conn->query($sqlInsert) === TRUE) {
-                #echo "Record updated successfully";
-            } else {
-                exit("Fehler beim Speichern der Daten: " . $conn->error);
-            }
-        }
-    }
-}
-*/
-
 
 if ($changes == true) {
     echo "<div class='alert alert-success'>Deine Daten wurden erfolgreich eingetragen. Du kannst sie dir <a href='index.php?mode=view'>hier</a> ansehen.</div>";
+
+    mail($email_to,$email_subject,$email_body, $email_header);
+
 } else {
 echo "<div class='alert alert-info'>Es wurden keine Änderungen vorgenommen. Du kannst dich <a href='index.php'>hier</a> eintragen.</div>";
 }
